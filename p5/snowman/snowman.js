654bc7b50dca65d817a3ca74f929a3ba0f01f8f7
nickpoole35 <63111815+nickpoole35@users.noreply.github.com>
2020-04-17 12:14:36 -0400

    Add files via upload

---
+++ p5/snowman/snowman.js
@@ -0,0 +1,12 @@
+function setup() {
+  				createCanvas(400, 400);
+			 }
+
+			function draw() {
+        rect(5,350,400,100); //gound
+        ellipse(200, 300, 150, 150); //bottom snowball
+        ellipse(200, 200, 100, 100); //middle snowball
+        ellipse(200, 120, 75, 75); //top snowball
+        line(160,200,75,150); //left arm
+        line(240,200,325,150);
+     }
